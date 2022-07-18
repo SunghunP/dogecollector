3fe8502c3531c182e9adb77f582befd2466daa0f
Sunghun Park <sunghun.josephp@gmail.com>
2022-07-17 22:54:42 -0700

    About page rendered

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+	return HttpResponse('<h1>Bruh This is the about page</h1>')
