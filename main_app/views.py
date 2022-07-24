1a4ef34babc1ebe3b3eeae2c5545e58cb3900051
Joe Park <sunghun.josephp@gmail.com>
2022-07-24 14:08:48 -0700

    Add in form to create a new dog

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Dog
 
 # Create your views here.
@@ -15,3 +17,8 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', {'dog':dog})
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'age', 'description']
+
