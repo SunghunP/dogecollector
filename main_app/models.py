1a9c4013c15c72643eff684e6e15c781d8a1a143
Joe Park <sunghun.josephp@gmail.com>
2022-07-25 04:54:53 -0700

    Able to associate Toys with Dogs

--- main_app/models.py
+++ main_app/models.py
@@ -3,12 +3,23 @@ from django.urls import reverse
 from datetime import date
 # Create your models here.
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=300)
   age = models.IntegerField()
-  
+  toys = models.ManyToManyField(Toy)
+
   def __str__(self):
     return f'Dog named {self.name}. Breed: {self.breed}. Age of: {self.age} '
 
@@ -40,12 +51,3 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
-class Toy(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
-
-  def __str__(self):
-    return self.name
-
-  def get_absolute_url(self):
-    return reverse('toys_detail', kwargs={'pk': self.id})
