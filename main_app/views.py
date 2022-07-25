7186895b42f8d2d286942a3f5956474ec8b34ce0
Joe Park <sunghun.josephp@gmail.com>
2022-07-25 04:33:50 -0700

    Add Toys model and full CRUD for toys

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from .models import Dog, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -42,3 +42,25 @@ def add_feeding(request, dog_id):
   new_feeding.dog_id = dog_id
   new_feeding.save()
   return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+  
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
