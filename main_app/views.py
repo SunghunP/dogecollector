0135264188b34d685408a853b8e52b247a71cedc
Joe Park <sunghun.josephp@gmail.com>
2022-07-25 05:10:02 -0700

    Change html and styling of toys list

--- main_app/views.py
+++ main_app/views.py
@@ -18,9 +18,11 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   feeding_form = FeedingForm()
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   return render(request, 'dogs/detail.html', {
     'dog':dog,
-    'feeding_form': feeding_form
+    'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have,
   })
 
 class DogCreate(CreateView):
@@ -42,7 +44,7 @@ def add_feeding(request, dog_id):
   new_feeding.dog_id = dog_id
   new_feeding.save()
   return redirect('detail', dog_id=dog_id)
-
+  
 def assoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
